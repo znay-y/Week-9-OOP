09f038c46a3ed006909e620c579a5689cac5b803
Z Laptop <zaineirshad.07@gmail.com>
2026-03-27 11:10:00 +0000

    Final version of question one

--- quest1.java
+++ quest1.java
@@ -6,7 +6,7 @@ public class quest1 {
     static String getElement(String[] array, int index) {
         try {
             return array[index];
-        } catch (Exception e) {
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("Index is out of array");
             return null;
         }
