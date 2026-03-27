1f4574eab2df3c883318ae3a5030d65734847fd1
Z Laptop <zaineirshad.07@gmail.com>
2026-03-27 11:10:08 +0000

    Final version of question three

--- quest3.java
+++ quest3.java
@@ -1,24 +1,20 @@
 
-
 public class quest3 {
-public static void main(String[] args) {
-    getStringLength(null);
-   
-}
+    public static void main(String[] args) {
+        getStringLength("null");
 
-
-static int getStringLength(String s){
-   try {
-        return s.length();
-    } catch (NullPointerException e) {
-        System.out.println("Null passed :(");
-        return 0;
     }
-    finally{
-        System.out.println("Execution completed");
-    }
-        
-     
-}
-}
 
+    static int getStringLength(String s) {
+        try {
+            System.out.println("The length is " + s.length());
+            return s.length();
+        } catch (NullPointerException e) {
+            System.out.println("Null passed :(");
+            return 0;
+        } finally {
+            System.out.println("Execution completed");
+        }
+
+    }
+}
